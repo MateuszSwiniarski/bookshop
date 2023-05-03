1dd2ec80954dd38a9c7b7640a0e7056c70450f5c
Mateusz Świnairski <eirdom@gmail.com>
2023-05-03 21:35:28 +0200

    M4L2 create CatalogController and first endpoint

---
+++ src/main/java/pl/rodzyn/bookshop/catalog/web/CatalogController.java
@@ -0,0 +1,22 @@
+package pl.rodzyn.bookshop.catalog.web;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.rodzyn.bookshop.catalog.application.port.CatalogUseCase;
+import pl.rodzyn.bookshop.catalog.domain.Book;
+
+import java.util.List;
+
+@RequestMapping("/catalog")
+@RestController
+@AllArgsConstructor
+public class CatalogController {
+    private final CatalogUseCase catalog;
+
+    @GetMapping
+    public List<Book> getAll(){
+        return catalog.findAll();
+    }
+}
