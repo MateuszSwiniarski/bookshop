7c8e8eeff9fd0d3aac183d14cc19ff3c8109bcb9
Mateusz Świnairski <eirdom@gmail.com>
2023-05-03 22:06:45 +0200

    M4L3 create findById endpoint

--- src/main/java/pl/rodzyn/bookshop/catalog/web/CatalogController.java
+++ src/main/java/pl/rodzyn/bookshop/catalog/web/CatalogController.java
@@ -2,6 +2,7 @@ package pl.rodzyn.bookshop.catalog.web;
 
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.rodzyn.bookshop.catalog.application.port.CatalogUseCase;
@@ -18,5 +19,10 @@ public class CatalogController {
     @GetMapping
     public List<Book> getAll(){
         return catalog.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Book findById(@PathVariable Long id){
+        return catalog.findById(id).orElse(null);
     }
 }
