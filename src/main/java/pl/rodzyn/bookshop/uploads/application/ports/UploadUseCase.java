f002914e9878863eb5cee5f790a462b323fb6864
Mateusz Świnairski <eirdom@gmail.com>
2023-05-09 20:09:36 +0200

    M4L13 serve file

--- src/main/java/pl/rodzyn/bookshop/uploads/application/ports/UploadUseCase.java
+++ src/main/java/pl/rodzyn/bookshop/uploads/application/ports/UploadUseCase.java
@@ -1,13 +1,19 @@
 package pl.rodzyn.bookshop.uploads.application.ports;
 
+import lombok.AllArgsConstructor;
 import lombok.Value;
 import pl.rodzyn.bookshop.uploads.domain.Upload;
+
+import java.util.Optional;
 
 public interface UploadUseCase {
 
     Upload save(SaveUploadCommand command);
 
+    Optional<Upload> getById(String id);
+
     @Value
+    @AllArgsConstructor
     class SaveUploadCommand {
         String filename;
         byte[] file;
