b1e8cabe2f97a5e5767af09a96d7c48e68e18dd0
Mateusz Świnairski <eirdom@gmail.com>
2023-05-13 21:27:34 +0200

    M5L8 ManyToOne

--- src/main/java/pl/rodzyn/bookshop/order/domain/Order.java
+++ src/main/java/pl/rodzyn/bookshop/order/domain/Order.java
@@ -29,6 +29,7 @@ public class Order {
     @JoinColumn(name = "order_id")
     private List<OrderItem> items;
 
+    @ManyToOne(cascade = CascadeType.ALL)
     private Recipient recipient;
 
     @CreatedDate
