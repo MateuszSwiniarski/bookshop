514bb623491cb132722e1b602b3e82c04ad6d6c6
Mateusz Świnairski <eirdom@gmail.com>
2023-05-16 20:20:54 +0200

    M6.14 adding admin controller

--- src/main/java/pl/rodzyn/bookshop/catalog/web/AdminController.java
+++ src/main/java/pl/rodzyn/bookshop/catalog/web/AdminController.java
@@ -1,11 +1,10 @@
-package pl.rodzyn.bookshop;
+package pl.rodzyn.bookshop.catalog.web;
 
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import pl.rodzyn.bookshop.catalog.application.port.CatalogUseCase;
-import pl.rodzyn.bookshop.catalog.application.port.CatalogUseCase.UpdateBookCommand;
 import pl.rodzyn.bookshop.catalog.db.AuthorJpaRepository;
 import pl.rodzyn.bookshop.catalog.domain.Author;
 import pl.rodzyn.bookshop.catalog.domain.Book;
@@ -14,24 +13,23 @@ import pl.rodzyn.bookshop.order.application.port.QueryOrderUseCase;
 import pl.rodzyn.bookshop.order.domain.OrderItem;
 import pl.rodzyn.bookshop.order.domain.Recipient;
 
+import javax.transaction.Transactional;
 import java.math.BigDecimal;
-import java.util.List;
 import java.util.Set;
 
-import static pl.rodzyn.bookshop.catalog.application.port.CatalogUseCase.*;
-import static pl.rodzyn.bookshop.order.application.port.ManipulateOrderUseCase.*;
-
-@Component
+@RestController
+@RequestMapping("/admin")
 @AllArgsConstructor
-public class ApplicationStartup implements CommandLineRunner {
+class AdminController {
 
     private final CatalogUseCase catalog;
     private final ManipulateOrderUseCase placeOrder;
     private final QueryOrderUseCase queryOrder;
     private final AuthorJpaRepository authorRepository;
 
-    @Override
-    public void run(String... args){
+    @PostMapping("/data")
+    @Transactional
+    public void initialize() {
         initData();
         placeOrder();
     }
@@ -53,20 +51,20 @@ public class ApplicationStartup implements CommandLineRunner {
                 .email("jan@kowalski.pl")
                 .build();
 
-        PlaceOrderCommand command = PlaceOrderCommand
+        ManipulateOrderUseCase.PlaceOrderCommand command = ManipulateOrderUseCase.PlaceOrderCommand
                 .builder()
                 .recipient(recipient)
-//                .item(new OrderItem(panTadeusz.getId(), 16))
-//                .item(new OrderItem(chlopi.getId(), 7))
+                .item(new OrderItem(effectiveJava.getId(), 16))
+                .item(new OrderItem(puzzlers.getId(), 7))
                 .build();
 
-        PlaceOrderResponse response = placeOrder.placeOrder(command);
+        ManipulateOrderUseCase.PlaceOrderResponse response = placeOrder.placeOrder(command);
         System.out.println("Created ORDER with id: " + response.getOrderId());
 
         //list all orders
         queryOrder.findAll()
                 .forEach(order -> System.out.println("GOT ORDER WITH TOTAL PRICE: " + order.totalPrice()
-                + " DETAILS: " + order));
+                        + " DETAILS: " + order));
     }
 
     private void initData() {
@@ -75,13 +73,13 @@ public class ApplicationStartup implements CommandLineRunner {
         authorRepository.save(joshua);
         authorRepository.save(neal);
 
-        CreateBookCommand effectiveJava = new CreateBookCommand(
+        CatalogUseCase.CreateBookCommand effectiveJava = new CatalogUseCase.CreateBookCommand(
                 "Effective Java",
                 Set.of(joshua.getId()),
                 2005,
                 new BigDecimal("79.00")
         );
-        CreateBookCommand javaPuzzlers = new CreateBookCommand(
+        CatalogUseCase.CreateBookCommand javaPuzzlers = new CatalogUseCase.CreateBookCommand(
                 "Java Puzzlers",
                 Set.of(joshua.getId(), neal.getId()),
                 2018,
