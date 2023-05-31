7f378ce71446dc5fb0425296bd88fbfb9a0a7a47
Mateusz Świnairski <eirdom@gmail.com>
2023-05-31 20:34:00 +0200

    M8l9 Test WEB layer

--- src/main/java/pl/rodzyn/bookshop/BookshopApplication.java
+++ src/main/java/pl/rodzyn/bookshop/BookshopApplication.java
@@ -11,7 +11,6 @@ import org.springframework.web.client.RestTemplate;
 import pl.rodzyn.bookshop.order.application.OrdersProperties;
 
 @EnableScheduling
-@EnableJpaAuditing
 @SpringBootApplication
 @EnableConfigurationProperties(OrdersProperties.class)
 public class BookshopApplication {
