e37addd0dab794911b05828c6a12a10dc4eb570d
Mateusz Świnairski <eirdom@gmail.com>
2023-06-14 21:27:25 +0200

    M9L4 user in memory

--- src/main/java/pl/rodzyn/bookshop/BookshopSecurityConfiguration.java
+++ src/main/java/pl/rodzyn/bookshop/BookshopSecurityConfiguration.java
@@ -2,6 +2,7 @@ package pl.rodzyn.bookshop;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -18,5 +19,17 @@ public class BookshopSecurityConfiguration extends WebSecurityConfigurerAdapter 
             .httpBasic()
         .and()
             .csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("marek@exapmle.org")
+                .password("{noop}xxx")
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password("{noop}xxx")
+                .roles("ADMIN");
     }
 }
